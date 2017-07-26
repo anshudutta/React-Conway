9ccdae57f284a3e201ef206798407069919f15bb
anshu.dutta@gmail.com <anshu.dutta@gmail.com>
2017-07-27 00:14:48 +1000

    added header

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Universe from '../containers/universe';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <Universe/>
       </div>
     );
